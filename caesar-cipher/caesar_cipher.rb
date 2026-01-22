225f119d645cfdd323a276714266d892f2c10a36
beenehdev <beenehdev@gmail.com>
2026-01-22 07:17:57 -0800

    Clean up comments

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -1,11 +1,3 @@
-# Project plan:
-# 1: Get string from user input of alphabetic character "string_input", and positive or negative number representing right or left shift "shift_value_input"
-# 2: Convert "string_input" to numeric values 1/26 representing alphabetic index "num_encoded_input", convert "shift_value_input" to integer
-# 3: Shift "num_encoded_input" "shift_value_input"
-# 4: Convert "num_encoded_output" to "string_output" from number using alphabetic index
-
-
-# 1 -> Grab user input, ensure user input validity (alphabet for 1st var, integer for 2nd)
 def get_user_input
   puts "Please enter a sentence, alphabetic characters only."
   string_input = gets.chomp
