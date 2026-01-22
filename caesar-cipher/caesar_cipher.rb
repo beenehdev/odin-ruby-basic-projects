a1cb99ed618bfcc6f0d1c75925633f390d103e05
beenehdev <beenehdev@gmail.com>
2026-01-21 18:23:47 -0800

    Refactor to fix logic and wrap in method

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -13,6 +13,7 @@ def get_user_input
   puts "Please enter a positive or negative integer up to 25.\nIt will represent how your sentence is shifted."
   shift_value_input = gets.chomp.to_i
   unless shift_value_input.between?(-25, 25)
+    puts "Invalid input detected: defaulting shift to 0."
     shift_value_input = 0
   end
 
@@ -28,25 +29,38 @@ def cipher_numcode_shifter(precoded_letter, shift_by_int)
   postcoded_letter
 end
 
-string_input, shift_value_input = get_user_input
-abc_array = ("a".."z").to_a
-result_array = Array.new
+def caesar_cipher(string_input, shift_value_input)
+  low_abc_array = ("a".."z").to_a
+  high_abc_array = ("A".."Z").to_a
+  
+  result_array = Array.new
 
-string_input.each_char do |ch|
-  was_upcase = false
-  if ch == ch.upcase then was_upcase = true end
-  lower = ch.downcase
+  string_input.each_char do |ch|
+    was_upcase = false
+    if high_abc_array.include?(ch) then was_upcase = true end
+      
+      unless low_abc_array.include?(ch) ||  high_abc_array.include?(ch)
+        result_array.push(ch)
+        next
+      end
+      
+    lower = ch.downcase
 
-  unless abc_array.include?(lower)
-    result_array.push(lower)
-    next
+    precoded_letter = low_abc_array.index(lower) + 1
+    postcoded_letter = cipher_numcode_shifter(precoded_letter, shift_value_input)
+
+    if was_upcase
+      result_letter = low_abc_array[postcoded_letter - 1].upcase
+    else
+      result_letter = low_abc_array[postcoded_letter - 1]
+    end
+
+    result_array.push(result_letter)
   end
 
-  precoded_letter = abc_array.index(lower) + 1
-  postcoded_letter = cipher_numcode_shifter(precoded_letter, shift_value_input)
-  # Something to reapply upcase if was_upcase = true
-  result_array.push(abc_array[postcoded_letter - 1])
+  result_array.join("")
 end
 
-string_result = result_array.join("")
+string_input, shift_value_input = get_user_input
+string_result = caesar_cipher(string_input, shift_value_input)
 puts string_result
