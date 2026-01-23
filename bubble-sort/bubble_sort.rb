bbde869db7f1e510962fa128fbe26534d0247258
beenehdev <beenehdev@gmail.com>
2026-01-22 18:49:17 -0800

    Add end of day progress for online storage

---
+++ bubble-sort/bubble_sort.rb
@@ -0,0 +1,33 @@
+#  max iterations = n - 1 n being list/array length
+#  after each iteration through array, only check length - m 
+#  
+#THIS SOLUTION DOESN'T WORK AT ALL LOL
+
+def bubble_sort(array)
+  n = array.length
+
+  n.times do 
+    array.each_index do |index|
+      next if index == array.length - 1
+
+      next_num = array[(index+1)]
+      current_num = array[index]
+
+      if current_num > next_num
+        current_num, next_num = next_num + current_num
+        array
+      end
+
+      array
+    end
+
+    array
+  end
+
+  puts array
+  array
+end
+
+a_very_bubbly_array = [42, 7, 89, 16, 63, 28, 91, 54, 3, 77, 35, 68, 12, 84, 26, 59, 98, 41, 9, 70]
+
+bubble_sort(a_very_bubbly_array)
