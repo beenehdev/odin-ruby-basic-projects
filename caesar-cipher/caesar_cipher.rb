041612c5e32bc723abbb3069e9e871d1d48b4f27
beenehdev <beenehdev@gmail.com>
2026-01-27 14:18:47 -0800

    Add linter and gem bundle tools

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -1,11 +1,13 @@
-def get_user_input
-  puts "Please enter a sentence, alphabetic characters only."
+# frozen_string_literal: true
+
+def user_input
+  puts 'Please enter a sentence, alphabetic characters only.'
   string_input = gets.chomp
 
   puts "Please enter a positive or negative integer up to 25.\nIt will represent how your sentence is shifted."
   shift_value_input = gets.chomp.to_i
   unless shift_value_input.between?(-25, 25)
-    puts "Invalid input detected: defaulting shift to 0."
+    puts 'Invalid input detected: defaulting shift to 0.'
     shift_value_input = 0
   end
 
@@ -14,45 +16,45 @@ end
 
 def cipher_numcode_shifter(precoded_letter, shift_by_int)
   postcoded_letter = precoded_letter + shift_by_int
-  
-  if postcoded_letter > 26 then postcoded_letter -= 26 end
-  if postcoded_letter < 1 then postcoded_letter +=  26 end
-  
+
+  postcoded_letter -= 26 if postcoded_letter > 26
+  postcoded_letter += 26 if postcoded_letter < 1
+
   postcoded_letter
 end
 
 def caesar_cipher(string_input, shift_value_input)
-  low_abc_array = ("a".."z").to_a
-  high_abc_array = ("A".."Z").to_a
-  
-  result_array = Array.new
+  low_abc_array = ('a'..'z').to_a
+  high_abc_array = ('A'..'Z').to_a
+
+  result_array = []
 
   string_input.each_char do |ch|
     was_upcase = false
-    if high_abc_array.include?(ch) then was_upcase = true end
-      
-      unless low_abc_array.include?(ch) ||  high_abc_array.include?(ch)
-        result_array.push(ch)
-        next
-      end
-      
+    was_upcase = true if high_abc_array.include?(ch)
+
+    unless low_abc_array.include?(ch) || high_abc_array.include?(ch)
+      result_array.push(ch)
+      next
+    end
+
     lower = ch.downcase
 
     precoded_letter = low_abc_array.index(lower) + 1
     postcoded_letter = cipher_numcode_shifter(precoded_letter, shift_value_input)
 
-    if was_upcase
-      result_letter = low_abc_array[postcoded_letter - 1].upcase
-    else
-      result_letter = low_abc_array[postcoded_letter - 1]
-    end
+    result_letter = if was_upcase
+                      low_abc_array[postcoded_letter - 1].upcase
+                    else
+                      low_abc_array[postcoded_letter - 1]
+                    end
 
     result_array.push(result_letter)
   end
 
-  result_array.join("")
+  result_array.join('')
 end
 
-string_input, shift_value_input = get_user_input
+string_input, shift_value_input = user_input
 string_result = caesar_cipher(string_input, shift_value_input)
 puts string_result
